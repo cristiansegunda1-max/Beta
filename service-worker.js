296c6353d48e82eb7aa28dbafd4ba350ebd04bba
cristiansegunda1-max <cristiansegunda1@gmail.com>
2026-02-14 03:05:19 -0500

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,41 @@
-const CACHE_NAME = "cmcard-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./logo.png",
+const CACHE_NAME = 'cm-card-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png',
+  './icon-192.png',
+  './icon-512.png',
   "./agregar.PNG",
   "./acciones.PNG",
   "./ajustes.PNG",
   "./moon.PNG",
-  "./sun.PNG",
-  "./apple-icon.png",
-  "./manifest.json"
+  "./sun.PNG"
+  
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
+
+  
+  
+  
+  
+  
+    "./index.html",
+  "./logo.png",
+  
+  "./apple-icon.png",
+  "./manifest.json"
+  
+  
+  
+  
